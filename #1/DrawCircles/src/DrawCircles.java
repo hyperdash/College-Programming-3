ca63f8077eb8cad9d2d121bb2325643224dbd2ef
Kazuhiro Kosaka <kazuhiro.kosaka@gmail.com>
2016-06-15 03:07:10 +0900

    Prep for #1

---
+++ #1/DrawCircles/src/DrawCircles.java
@@ -0,0 +1,8 @@
+import java.applet.Applet;
+
+/**
+ * @author 15T026en@stu.teikyo-u.ac.jp
+ *         Created on 6/15/16.
+ */
+public class DrawCircles extends Applet {
+}
---
+++ #1/LowerCase/src/LowerCase.java
@@ -0,0 +1,14 @@
+import java.applet.Applet;
+import java.awt.*;
+
+/**
+ * @author 15T026en@stu.teikyo-u.ac.jp
+ *         Created on 6/15/16.
+ */
+public class LowerCase extends Applet {
+
+    public void paint(Graphics g) {
+
+    }
+
+}
